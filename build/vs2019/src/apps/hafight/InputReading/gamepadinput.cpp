91e0864157a15deafdd5ec58b1fdb2ce343c0eb7
Eric Yoon <eyoon525@gmail.com>
2022-01-18 13:27:46 -0800

    input reader

---
+++ build/vs2019/src/apps/hafight/InputReading/gamepadinput.cpp
@@ -0,0 +1,20 @@
+/*******************************************************************************************
+*
+*   raylib [core] example - Gamepad input
+*
+*   NOTE: This example requires a Gamepad connected to the system
+*         raylib is configured to work with the following gamepads:
+*                - Xbox 360 Controller (Xbox 360, Xbox One)
+*                - PLAYSTATION(R)3 Controller
+*         Check raylib.h for buttons configuration
+*
+*   This example has been created using raylib 2.5 (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2013-2019 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+
+// NOTE: Gamepad name ID depends on drivers and OS
--- build/vs2019/src/apps/hafight/MainGame/inputreader.cpp
+++ build/vs2019/src/apps/hafight/MainGame/inputreader.cpp
@@ -23,12 +23,34 @@ ReadInputs()
        { KEY_J,         INPUT_FIRE },
        { KEY_K,         INPUT_BOMB },
     };
+    static const struct {
+        int      key;
+        int      input;
+    } padinputtable[] = {
+       { 1,       INPUT_THRUST },
+       { 3,     INPUT_BREAK },
+       { 4,     INPUT_ROTATE_LEFT },
+       { 2,    INPUT_ROTATE_RIGHT },
+       { 8,         INPUT_FIRE },
+       { 5,         INPUT_BOMB },
+    };
     int i, inputs = 0;
 
-    for (i = 0; i < (int)(sizeof(inputtable) / sizeof(inputtable[0])); i++) {
-        if (IsKeyDown(inputtable[i].key)) {
-            inputs |= inputtable[i].input;
+    if (IsGamepadAvailable(0)) {
+        for (i = 0; i < (int)(sizeof(padinputtable) / sizeof(padinputtable[0])); i++) {
+            if (GetGamepadButtonPressed() == (padinputtable[i].key)) {
+                inputs |= padinputtable[i].input;
+            }
         }
     }
+    else {
+        for (i = 0; i < (int)(sizeof(inputtable) / sizeof(inputtable[0])); i++) {
+            if (IsKeyDown(inputtable[i].key)) {
+                inputs |= inputtable[i].input;
+            }
+        }
+    }
+
+    
     return inputs;
 }
