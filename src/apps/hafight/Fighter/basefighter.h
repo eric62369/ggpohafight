4f6cdf7aef2a36af4c2cc04599f03146c820b010
Eric Yoon <eyoon525@gmail.com>
2022-02-02 17:35:33 -0800

    rebuilds safely

---
+++ src/apps/hafight/Fighter/basefighter.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef _BASEFIGHTER_H_
+#define _BASEFIGHTER_H_
+
+#include "inputinterpreter.h"
+#include "../FighterStates/fighterstate.h"
+#include "../gamestate.h"
+#include "../Physics/rigidbody.h"
+
+namespace Player {
+    class FighterState;
+
+    class BaseFighter {
+    public:
+        BaseFighter(Fighter* _gameStateData);
+        ~BaseFighter();
+        void HandleInput(InputAction input);
+        void Update();
+        void LoadState(int stateEnum, int frame);
+
+        Physics::Rigidbody _rb;
+
+    private:
+        void SaveState();
+
+        Fighter* _gameStateData;
+        FighterState* _state;
+    };
+}
+
+#endif
---
+++ src/apps/hafight/Fighter/inputinterpreter.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef _INPUTINTERPRETER_H_
+#define _INPUTINTERPRETER_H_
+
+#include "../inputreader.h"
+
+namespace Player {
+    enum class InputAction {
+        Forward,
+        Backward,
+        Neutral,
+    };
+
+    class InputInterpreter {
+    public:
+        InputInterpreter() {
+        }
+        InputAction ParseInput(int inputs) {
+            if (inputs & INPUT_ROTATE_RIGHT) {
+                return InputAction::Forward;
+            }
+            else if (inputs & INPUT_ROTATE_LEFT) {
+                return InputAction::Backward;
+            }
+            else {
+                return InputAction::Neutral;
+            }
+        }
+    };
+}
+
+#endif
